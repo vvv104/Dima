c6b0b92744412eb00535258365ef87bcae3437db
Vladimir Voronkov <voronkovv@mail.ru>
2022-08-01 09:38:13 +0300

    Day2

--- NiceHexSpiral.py
+++ NiceHexSpiral.py
@@ -1,9 +1,9 @@
 import turtle
-colors =['red','purple','blue','green','yellow','orange']
+colors =['red','orange','yellow','green','light blue','blue','purple']
 t = turtle.Pen()
 turtle.bgcolor('black')
 for x in range (360) :
-    t.pencolor(colors[x%6])
+    t.pencolor(colors[x%7])
     t.width(x/100+1)
     t.forward(x)
-    t.left(59)
+    t.left(100)
