644383a9ab1c034d9998b9a6da7dec5112570536
Dmitry Voronkov <dmitr.voronkov@gmail.com>
2022-08-13 14:47:47 +0300

    Day4

--- ColorSpiral.py
+++ ColorSpiral.py
@@ -2,7 +2,7 @@ from multiprocessing.forkserver import SIGNED_STRUCT
 import turtle
 t = turtle.Pen()
 turtle.bgcolor("black")
-sides = 1
+sides = 6
 colors =["red","yellow","blue","orange","green","purple"]
 for x in range(360) :
     t.pencolor(colors[x%sides])
---
+++ ThankYou.py
@@ -0,0 +1,7 @@
+my_name = "Брайсон"
+my_age = 43
+your_name =input ("Как тебя зовут?")
+your_age= input ("сколько тебе лет?")
+print("меня зовут",my_name,"и мне",my_age,"лет.")
+print("тебя зовут",your_name,"и тебе ",your_age,"лет")
+print("спасибо за покупку книги.",your_name,"!")
